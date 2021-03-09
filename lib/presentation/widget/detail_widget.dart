9d31bcfc917b8a640639aafead3a872f2f90c95f
juan <jr90lp@gmail.com>
2021-03-09 20:58:55 -0300

    complete pokemon detail UI

--- lib/presentation/widget/detail_widget.dart
+++ lib/presentation/widget/detail_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedex_app/domain/model/pokemon.dart';
 import 'package:pokedex_app/presentation/bloc/pokemon_detail_bloc.dart';
 import 'package:pokedex_app/presentation/bloc/pokemon_state.dart';
+import 'package:pokedex_app/presentation/widget/rounded_container_widget.dart';
 import 'package:provider/provider.dart';
 import 'stat_widget.dart';
 
@@ -34,12 +35,48 @@ class _DetailState extends State<DetailWidget> {
             child: CircularProgressIndicator(),
           );
         else if (bloc.state is Successful) {
+          final Successful successfulState = bloc.state;
+          final pokemonDetail = successfulState.pokemon;
           return Container(
             color: Colors.black12,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                  flex: 3,
+                  child: Container(
+                    margin: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      pokemonDetail.name,
+                      style: DefaultTextStyle.of(context)
+                          .style
+                          .apply(fontSizeFactor: 2.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: pokemonDetail.types
+                          .map((e) => RoundedContainerWidget(e.name))
+                          .toList(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      'Base Stats',
+                      style: DefaultTextStyle.of(context)
+                          .style
+                          .apply(fontSizeFactor: 1.5),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
